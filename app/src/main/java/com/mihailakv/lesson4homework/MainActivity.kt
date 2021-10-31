d060761fb5b5c04134d3c293a23f85ea81d029a7
Mihail Akv <mihail_akv@mail.ru>
2021-10-31 19:52:01 +0300

    final version of homework

--- app/src/main/java/com/mihailakv/lesson4homework/MainActivity.kt
+++ app/src/main/java/com/mihailakv/lesson4homework/MainActivity.kt
@@ -10,20 +10,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //val weHaveAnAlphabetHereTextView: TextView = findViewById(R.id.we_have_an_alphabet_here)
+        val weHaveAnAlphabetHereTextView: TextView = findViewById(R.id.we_have_an_alphabet_here)
         val alphabetTextView: TextView = findViewById(R.id.alphabet_text_view)
         val showEvenOnlyButton: Button = findViewById(R.id.show_even_only_button)
+        var evenLetters = ""
 
-        showEvenOnlyButton.setOnClickListener () {
+
             val alphabetList = listOf('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
-                .forEachIndexed { index, value ->
+                alphabetList.forEachIndexed { index, element ->
                     when {
                         (index % 2 != 0) ->
-                    /*weHaveAnAlphabetHereTextView.text =
-                        ("...And these are even letters of the alphabet")**/
-                    alphabetTextView.text = value.toString()
-                }
+                        evenLetters = "$evenLetters $element"
+                        }
             }
+        showEvenOnlyButton.setOnClickListener () {
+            alphabetTextView.text = evenLetters
+            weHaveAnAlphabetHereTextView.text = "...And these are even letters of the alphabet"
         }
     }
 }
